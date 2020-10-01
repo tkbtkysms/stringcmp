bd4170e8c8b5d7c2402145601573d8c6cb35255d
Yoshimasa Takabatake <tkbtkysms@tkbtkMBA.local>
2020-10-01 14:05:20 +0900

    change std::__gcd to std::gcd

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -1,95 +1,119 @@
-#include"Fraction.hpp"
-#include <iostream>
-#include <stdlib.h>
+#include "Fraction.hpp"
 #include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <stdlib.h>
 
-Fraction::Fraction(){
-  denominator = 0;
-  numerator = 0;
+Fraction::Fraction()
+{
+    denominator = 0;
+    numerator = 0;
 }
-Fraction Fraction::product(Fraction frac){
-  Fraction answer;
-  if(denominator != 0 && frac.getdenominator() != 0){
-    int tmp_denominator = denominator*frac.getdenominator();
-    int tmp_numerator = numerator*frac.getnumerator();
-    int gcd = std::__gcd(abs(tmp_numerator),tmp_denominator);
-    answer.set(tmp_numerator/gcd,tmp_denominator/gcd);
-  }
-  else{
-    answer.set(0,0);
-  }
-  return answer;
-}
-
-Fraction Fraction::subtract(Fraction frac){
-  Fraction answer;
-  int tmp_numerator1,tmp_numerator2;
-  int new_numerator;
-  if(denominator == 0 && frac.getdenominator() == 0){
-    answer.set(0,0);
-  }
-  else if(denominator == 0 && frac.getdenominator() != 0){
-    answer.set(frac.getnumerator(),frac.getdenominator());
-  }
-  else if(denominator != 0 && frac.getdenominator() == 0){
-    answer.set(numerator,denominator);
-  }
-  else{
-    int lcm1 = lcm(denominator,frac.getdenominator());
-    tmp_numerator1 = numerator*(lcm1/denominator);
-    tmp_numerator2 = frac.getnumerator()*(lcm1/frac.getdenominator());
-    if((new_numerator = tmp_numerator1 - tmp_numerator2) == 0){
-      answer.set(0,0);
+Fraction Fraction::product(Fraction frac)
+{
+    Fraction answer;
+    if(denominator != 0 && frac.getdenominator() != 0)
+    {
+        int tmp_denominator = denominator * frac.getdenominator();
+        int tmp_numerator = numerator * frac.getnumerator();
+        int gcd = std::gcd(abs(tmp_numerator), tmp_denominator);
+        answer.set(tmp_numerator / gcd, tmp_denominator / gcd);
     }
-    else{
-      int gcd = std::__gcd(new_numerator,lcm1);
-      answer.set(new_numerator/gcd,lcm1/gcd);
+    else
+    {
+        answer.set(0, 0);
     }
-  }
-  return answer;
+    return answer;
 }
 
-Fraction Fraction::add(Fraction frac){
-  Fraction answer;
-  int tmp_numerator1,tmp_numerator2;
-  int new_numerator;
-  if(denominator == 0 && frac.getdenominator() == 0){
-    answer.set(0,0);
-  }
-  else if(denominator == 0 && frac.getdenominator() != 0){
-    answer.set(frac.getnumerator(),frac.getdenominator());
-  }
-  else if(denominator != 0 && frac.getdenominator() == 0){
-    answer.set(numerator,denominator);
-  }
-  else{
-    int lcm1 = lcm(denominator,frac.getdenominator());
-    tmp_numerator1 = numerator*(lcm1/denominator);
-    tmp_numerator2 = frac.getnumerator()*(lcm1/frac.getdenominator());
-    if((new_numerator = tmp_numerator1 + tmp_numerator2) == 0){
-      answer.set(0,0);
+Fraction Fraction::subtract(Fraction frac)
+{
+    Fraction answer;
+    int tmp_numerator1, tmp_numerator2;
+    int new_numerator;
+    if(denominator == 0 && frac.getdenominator() == 0)
+    {
+        answer.set(0, 0);
     }
-    else{
-      int gcd = std::__gcd(abs(new_numerator),lcm1);
-      answer.set(new_numerator/gcd,lcm1/gcd);
+    else if(denominator == 0 && frac.getdenominator() != 0)
+    {
+        answer.set(frac.getnumerator(), frac.getdenominator());
     }
-  }
-  return answer;
+    else if(denominator != 0 && frac.getdenominator() == 0)
+    {
+        answer.set(numerator, denominator);
+    }
+    else
+    {
+        int lcm1 = lcm(denominator, frac.getdenominator());
+        tmp_numerator1 = numerator * (lcm1 / denominator);
+        tmp_numerator2 = frac.getnumerator() * (lcm1 / frac.getdenominator());
+        if((new_numerator = tmp_numerator1 - tmp_numerator2) == 0)
+        {
+            answer.set(0, 0);
+        }
+        else
+        {
+            int gcd = std::gcd(new_numerator, lcm1);
+            answer.set(new_numerator / gcd, lcm1 / gcd);
+        }
+    }
+    return answer;
 }
 
-void Fraction::set(int new_numerator, int new_denominator){
-  numerator = new_numerator;
-  denominator = new_denominator;
+Fraction Fraction::add(Fraction frac)
+{
+    Fraction answer;
+    int tmp_numerator1, tmp_numerator2;
+    int new_numerator;
+    if(denominator == 0 && frac.getdenominator() == 0)
+    {
+        answer.set(0, 0);
+    }
+    else if(denominator == 0 && frac.getdenominator() != 0)
+    {
+        answer.set(frac.getnumerator(), frac.getdenominator());
+    }
+    else if(denominator != 0 && frac.getdenominator() == 0)
+    {
+        answer.set(numerator, denominator);
+    }
+    else
+    {
+        int lcm1 = lcm(denominator, frac.getdenominator());
+        tmp_numerator1 = numerator * (lcm1 / denominator);
+        tmp_numerator2 = frac.getnumerator() * (lcm1 / frac.getdenominator());
+        if((new_numerator = tmp_numerator1 + tmp_numerator2) == 0)
+        {
+            answer.set(0, 0);
+        }
+        else
+        {
+            int gcd = std::gcd(abs(new_numerator), lcm1);
+            answer.set(new_numerator / gcd, lcm1 / gcd);
+        }
+    }
+    return answer;
 }
 
-int Fraction::getnumerator(){
-  return numerator;
+void Fraction::set(int new_numerator, int new_denominator)
+{
+    numerator = new_numerator;
+    denominator = new_denominator;
 }
 
-int Fraction::getdenominator(){
-  return denominator;
+int Fraction::getnumerator()
+{
+    return numerator;
 }
 
-int Fraction::lcm(int a, int b){
-  return a*b/std::__gcd(a,b);
+int Fraction::getdenominator()
+{
+    return denominator;
+}
+
+int Fraction::lcm(int a, int b)
+{
+    return a * b / std::gcd(a, b);
 }
